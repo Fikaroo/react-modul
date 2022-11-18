3039912294f58f2a2dce3ce4f8114ce4d5e32e1c
Fuad <fuadbayramv@gmail.com>
2022-11-18 18:21:30 +0400

    task-7

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import HospitalForm from "./components/HospitalForm";
+import Form from "./components/Form";
 
 function App() {
   return (
     <div className="App">
-      <HospitalForm />
+      <Form />
     </div>
   );
 }
